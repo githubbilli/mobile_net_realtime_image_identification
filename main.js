0ce140a1fc4f6dd185e967ccbd0f0c9249595da4
githubbilli <78857712+githubbilli@users.noreply.github.com>
2021-10-25 21:19:49 +0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,26 @@
+function setup() {
+  canvas = createCanvas(250, 250);
+  canvas.center();
+  video = createCapture(VIDEO);
+  video.hide();
+  classifier = ml5.imageClassifier('MobileNet',modelLoaded);
+}
+
+function draw() {
+  image(video,0,0,250,250);
+  classifier.classify(video,gotResult);
+}
+
+function modelLoaded() {
+  console.log("The model is loaded");
+}
+
+function gotResult(error,results) {
+  if(error){
+    console.error(error);
+  }else{
+    console.log(results);
+    document.getElementById("result_object_name").innerHTML = results[0].label;
+    document.getElementById("result_object_accuracy").innerHTML = results[0].confidence.toFixed(3);
+  }
+}
